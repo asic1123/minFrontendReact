32b4a7e039126f682b1fe587a5f4eb3398b07f93
eric <eric@xxx.com>
2022-05-09 21:16:16 +0800

    note:should manualy modified json file of chainID=3 and transaction hash and contract address

---
+++ minFrontendReact/frontend/src/ethereum.js
@@ -0,0 +1,25 @@
+import detectEthereumProvider from '@metamask/detect-provider';
+import { ethers, Contract } from 'ethers';
+import SimpleStorage from './contracts/SimpleStorage.json';
+
+const getBlockchain = () =>
+  new Promise( async (resolve, reject) => {
+    let provider = await detectEthereumProvider();
+    if(provider) {
+      await provider.request({ method: 'eth_requestAccounts' });
+      const networkId = await provider.request({ method: 'net_version' })
+      provider = new ethers.providers.Web3Provider(provider);
+      const signer = provider.getSigner();
+      console.log(SimpleStorage.networks[networkId].address);
+      const simpleStorage = new Contract(
+        SimpleStorage.networks[networkId].address,
+        SimpleStorage.abi,
+        signer
+      );
+      resolve({simpleStorage});
+      return;
+    }
+    reject('Install Metamask');
+  });
+
+export default getBlockchain;
